69db011884f66d0a20fb9412e71630683271d0d1
m.tsyvis <m.tsyvis@a1qa.com>
2021-07-27 18:09:56 +0300

    Added new architecture

---
+++ pages/main_page.py
@@ -0,0 +1,12 @@
+from .base_page import BasePage
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click() 
