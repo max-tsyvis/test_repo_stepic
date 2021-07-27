b5df3a88cd701ac461578789796725eb4b994984
m.tsyvis <m.tsyvis@a1qa.com>
2021-07-27 18:22:07 +0300

    added asset method

--- pages/main_page.py
+++ pages/main_page.py
@@ -10,3 +10,6 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click() 
+
+    def should_be_login_link(self):
+        assert self.if_elememt_present(By.CSS_SELECTOR,"#login_link"), "Login link is not presented"
