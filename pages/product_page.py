aaa2d34915b2f6f8e7d0ddaf45e8476f369a4524
m.tsyvis <m.tsyvis@a1qa.com>
2021-07-30 14:01:22 +0300

    add xfail mark

--- pages/product_page.py
+++ pages/product_page.py
@@ -15,8 +15,12 @@ class ProductPage(BasePage):
 
     def should_be_success_message(self, book_title):
         assert self.if_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Book isn't added to cart successfully"
-        assert book_title in self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text, "That's wrong book"
+        assert f"{book_title} has been added to your basket." in self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text, "That's wrong book"
 
     def should_be_total_amount(self, book_cost):
         assert self.if_element_present(*ProductPageLocators.TOTAL_COUNT_MESSAGE), "Total amount message didn't apear"
         assert book_cost in self.browser.find_element(*ProductPageLocators.TOTAL_COUNT_MESSAGE).text, "Total ammount is wrong"        
+
+    def get_book_name(self):
+        book = self.browser.find_element(By.TAG_NAME,"h1")
+        return book.text
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,26 @@
+from time import time
+import pytest
+import time
 from .pages.locators import ProductPageLocators
 from .pages.product_page import ProductPage
 
-def test_guest_can_add_product_to_cart(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    book_cost = '9.99'
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+
+def test_guest_can_add_product_to_cart(browser, link):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8"
+    book_cost = '19.99'
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_cart()
-    product_page.should_be_success_message(ProductPageLocators.BOOK_NAME)
+    site_book_name = product_page.get_book_name()
+    product_page.should_be_success_message(site_book_name)
     product_page.should_be_total_amount(book_cost)
